a466a4e96dfcf275fb3cfd84d86fc6f1f7916e02
KrishnaBir <krishnabir1996@gmail.com>
2025-10-13 07:30:34 +0530

    email input

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -8,6 +8,24 @@ function Banner() {
           <div className='space-y-12'>
             <h1 className='text-4xl font-bold'>Hello, welcomes here to learn something <span className='text-pink-500'>new everyday!!!!</span> </h1>
             <p className='text-xl'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita quaerat cumque illo veniam, repellendus impedit hic porro temporibus aperiam laboriosam, culpa vitae iure unde?</p>
+
+            <div><label className="input validator w-full">
+              <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                </g>
+              </svg>
+              <input type="email" placeholder="Email" required />
+            </label>
+              <div className="validator-hint hidden">Enter valid email address</div>
+            </div>
           </div>
         </div>
         <div className='w-full md:w-1/2'>right</div>
