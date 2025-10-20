a0830c26449c27d1a075ba5e9606e9d861ae660d
krishna <jhonhancock99@gmail.com>
2025-10-21 04:51:18 +0530

    file arrangement for routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,12 @@
 import React from 'react'
-import Navbar from './components/navbar.jsx'
-import Banner from './components/Banner.jsx'
-import Temp from './components/temp.jsx'
-import Footer from './components/Footer.jsx'
-import Freebook from './components/Freebook.jsx'
-
+import Home from './components/home/home.jsx'
+import Course from './components/Course.jsx'
 
 const App = () => {
   return (
     <>
-    <Navbar/>
-    <Banner/>
-    <Freebook/>
-    <Footer/>
-
-
-    
+      <Home/>
+      <Course/>
     {/* <Temp/> */}
     </>
   )
---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../navbar'
+import Banner from '../Banner'
+import Freebook from '../Freebook'
+import Footer from '../Footer'
+
+function Home() {
+  return (
+    <>
+    <Navbar/>
+    <Banner/>
+    <Freebook/>
+    <Footer/>
+
+    </>
+  )
+}
+
+export default Home
