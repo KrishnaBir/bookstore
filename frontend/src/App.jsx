bc3dc36faf2eff6c9652775113b5ff43b0e331bf
krishna <jhonhancock99@gmail.com>
2025-10-21 06:34:26 +0530

    routing: name, courses

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Home from './components/home/home.jsx'
 import Course from './components/Course.jsx'
+import { Route, Routes } from "react-router-dom"
 
 const App = () => {
   return (
     <>
-      <Home/>
-      <Course/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/course' element={<Course/>} />
+      </Routes>
+
+
     {/* <Temp/> */}
     </>
   )
