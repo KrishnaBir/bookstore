b6ed75be6595bd5dc19cfa5b9457d96adcfb03e4
krishna <jhonhancock99@gmail.com>
2025-10-26 05:13:37 +0530

    login modal complete

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Login() {
-    
+
     return (
         <div>
             <dialog id="my_modal_3" className="modal">
@@ -10,8 +10,31 @@ function Login() {
                         {/* if there is a button in form, it will close the modal */}
                         <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                     </form>
-                    <h3 className="font-bold text-lg">Hello!</h3>
-                    <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                    <h3 className="font-bold text-lg">login</h3>
+                    
+                    <div className='mt-4 space-y-2'>
+                        <span>Email</span> 
+                        <br/>
+                        <input type="email"
+                        placeholder="Enter your email"
+                        className='w-80 px-3 py-1 border outline-none rounded-md'
+                        />                           
+                    </div>
+
+                    <div className='mt-4 space-y-2'>
+                        <span>Password</span> 
+                        <br/>
+                        <input type="text"
+                        placeholder="Enter your password"
+                        className='w-80 px-3 py-1 border outline-none rounded-md'
+                        />   
+                    </div>
+
+                    <div className='flex justify-around mt-4'>
+                        <button className='bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200'>Login</button>
+                        <p>not register? <span className='underline text-blue-500 cursor-pointer'>Signup</span> </p>
+                    </div>
+
                 </div>
             </dialog>
         </div>
@@ -19,3 +42,4 @@ function Login() {
 }
 
 export default Login
+ 
